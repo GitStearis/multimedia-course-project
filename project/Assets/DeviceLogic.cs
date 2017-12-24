898463c846bf8bf0fbc8205928f5c5f684e8e05d
George Puisha <stearis@tut.by>
2017-12-24 06:07:21 +0300

    Add Voltage calculating

--- project/Assets/DeviceLogic.cs
+++ project/Assets/DeviceLogic.cs
@@ -5,12 +5,13 @@ using System.Collections;
 public class DeviceLogic : MonoBehaviour {
 
 	public int CurrentMode = 1;
+	public int ActiveDiode = 0;
 
 	public float Voltage = 0;
 	public float Amperage = 0;
 	public float Temperature = 293f;
 
-	public float[] StartStaticResistance = { 3000f, 3500f, 4000f, 4500f, 5000f };
+	public float[] StartStaticResistance = new float[5];
 
 	private float ButtonStartElevation = 0;
 	private float ButtonDeep = 0.02f;
@@ -23,6 +24,11 @@ public class DeviceLogic : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		GameObject.Find ("Mode").GetComponent<Text> ().text = CurrentMode.ToString();
+
+		for (int i = 0; i < 5; i++)
+		{
+			StartStaticResistance[i] = 3000 + 500 * i;
+		}
 
 		ButtonStartElevation = GameObject.Find("Diode-1").transform.position.y; 
 
@@ -53,11 +59,13 @@ public class DeviceLogic : MonoBehaviour {
 			button.transform.position = new Vector3 (button.transform.position.x, button.transform.position.y - ButtonDeep, button.transform.position.z);
 		} else {
 			button.transform.position = new Vector3 (button.transform.position.x, button.transform.position.y + ButtonDeep, button.transform.position.z);
+			ActiveDiode = 0;
 		}
 	}
 
 	public void UnpushAllButtons() {
 		GameObject button = new GameObject();
+
 		for (int i = 1; i <= 5; i++) {
 			button = GameObject.Find("Diode-" + i);
 			button.transform.position = new Vector3(button.transform.position.x, ButtonStartElevation, button.transform.position.z);
@@ -66,10 +74,13 @@ public class DeviceLogic : MonoBehaviour {
 
 	public void MoveReostat(GameObject reostat, float ReostatStartPosition) {
 		GameObject toggle = reostat.transform.Find ("Guide").gameObject.transform.Find ("Toggle").gameObject;
+
 		if (toggle.transform.position.x > ReostatStartPosition - ReostatLength + ReostatStep) {
 			toggle.transform.position = new Vector3 (toggle.transform.position.x - ReostatStep, toggle.transform.position.y, toggle.transform.position.z);
+			Voltage += 0.1f;
 		} else {
 			toggle.transform.position = new Vector3 (ReostatStartPosition, toggle.transform.position.y, toggle.transform.position.z);
+			Voltage = 0;
 		}
 	}
 
@@ -86,29 +97,48 @@ public class DeviceLogic : MonoBehaviour {
 		tumbler.transform.Find("Spinner").gameObject.transform.Rotate (0, 120f, 0);
 	}
 
+	public void Execute() {
+		if (ActiveDiode != 0)
+		{
+			Amperage = Voltage / StartStaticResistance [ActiveDiode - 1];
+		}
+		else 
+		{
+			Amperage = 0;
+		}
+		Debug.Log (Amperage);
+	}
+
 	public void HandleInteraction(GameObject tumbler){
 		switch (tumbler.name) {
 		case "Tumbler-1": {
 			SpinModeTumbler(tumbler);
+			UnpushAllButtons();
 			NextMode();
 		} break;
 		case "Diode-1": {
+			ActiveDiode = 1;
 			PushButton(tumbler);
 		} break;
 		case "Diode-2": {
+			ActiveDiode = 2;
 			PushButton(tumbler);
 		} break;
 		case "Diode-3": {
+			ActiveDiode = 3;
 			PushButton(tumbler);
 		} break;
 		case "Diode-4": {
+			ActiveDiode = 4;
 			PushButton(tumbler);
 		} break;
 		case "Diode-5": {
+			ActiveDiode = 5;
 			PushButton(tumbler);
 		} break;
 		case "Reostat-1": {
 			MoveReostat(tumbler, Reostat1StartPosition);
+			Execute();
 		} break;
 		case "Reostat-2": {
 			MoveReostat(tumbler, Reostat2StartPosition);
